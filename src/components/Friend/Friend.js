e1b30d877bd718b5b83cf0eb3c7844cdac27ffb9
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-01 16:27:43 +0600

    navigate friend detail

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Friend = ({friend}) => {
-    const {name, username} = friend;
+    const {name, username, id} = friend;
+    
+    const navigate = useNavigate();
+
+    const showFriendDetail = () => {
+        const path = `/friend/${id}`;
+        navigate(path);
+        // navigate(`/friend/${id}`); //direct navigate
+    }
     return (
         <div>
             <h2>Name: {name}</h2>
-            <button>{username}</button>
+            <button onClick={showFriendDetail}>{username}:{id}</button>
         </div>
     );
 };
